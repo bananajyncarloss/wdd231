40f4cc455200d18ee299db15e05eeb80878ba9ab
bananajyncarloss <banana.jyncarloss@gmail.com>
2026-03-19 18:22:12 +0800

    fixed dates.js

--- chamber/scripts/dates.js
+++ chamber/scripts/dates.js
@@ -1,2 +1,10 @@
-document.querySelector("#currentyear").textContent = new Date().getFullYear();
-document.querySelector("#lastModified").textContent = "Last Modified: " + document.lastModified;
+const year = document.querySelector("#year");
+const lastModified = document.querySelector("#lastModified");
+
+if (year) {
+  year.textContent = new Date().getFullYear();
+}
+
+if (lastModified) {
+  lastModified.textContent = "Last Modified: " + document.lastModified;
+}
