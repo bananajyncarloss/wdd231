b5fdec7281798029747a0ca5a54cf6b22a4e9611
bananajyncarloss <banana.jyncarloss@gmail.com>
2026-03-19 17:25:55 +0800

    Fixed some of the navigation problems

---
+++ chamber/scripts/navigation.js
@@ -0,0 +1,7 @@
+const menubutton = document.getElementById("#menu-toggle");
+const nav = document.getElementById("#nav");
+
+menubutton.addEventListener("click", () => {
+    menubutton.classList.toggle("open");
+    nav.classList.toggle("open");
+});
